fc395595e1a99491d47a148acf2e1f384c974dcb
Heich17SPL <janne.heinikoski@gmail.com>
2018-11-19 14:26:57 +0200

    Added security/filtering for the input values

--- WebServer/Versio1.php
+++ WebServer/Versio1.php
@@ -1,8 +1,9 @@
 <?php
 
+
 $servername = "localhost";	//Localhost, koska nykyinen palvelin vielä Raspberry Pi:llä.
-$username = "nettisivu"; 	//Vain INSERT -oikeuksilla
-$password = "Marraskuu5";
+$username = "nettisivu"; 	// Kyseisellä käyttäjällä ainoastaa
+$password = "Marraskuu5";	// INSERT -oikeudet
 $dbname = "GPS";
 
 							//Luo yhteys
@@ -12,23 +13,39 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-							//Jos saatu HTTP-viesti sisältää vastaavat muuttujat-> lue ne .php-sivulla
+
+
 if( $_GET["Lon"] || $_GET["Lat"] || $_GET["Speed"] ) 
 	{ 
-      echo "Longitude: ". $_GET['Lon'];
+     
+	$Lon = $_GET ['Lon'];
+	$Lat = $_GET ['Lat'];
+	$Speed = $_GET ['Speed'];
+	
+	$sec_Lon = $conn->real_escape_string($Lon);
+	$sec_Lat = $conn->real_escape_string($Lat);
+	$sec_Speed = $conn->real_escape_string($Speed);
+	
+	//$title = $db->real_escape_string($title)
+     
+      echo "Longitude: ". $sec_Lon;
       echo "<br>";
-      echo "Latitude: ". $_GET['Lat'];
+      echo "Latitude: ". $sec_Lat;
       echo "<br>";
-      echo "Speed: ". $_GET['Speed']; 
+      echo "Speed: ". $sec_Speed; 
 	} 
 
-$sql = "INSERT INTO gps_taulu (Lon, Lat, Speed) VALUES ('".$_GET["Lon"]."','".$_GET["Lat"]."','".$_GET["Speed"]."')";
+$sql = "INSERT INTO gps_taulu (Lon, Lat, Speed)
+		VALUES ('".$sec_Lon."','".$sec_Lat."','".$sec_Speed."')";
+		
 
 if ($conn->query($sql) === TRUE) {
-    echo "Uusi GPS-paikannus";
+    echo "Tiedot meni läpi";
 } else {
-    echo "Virhe: " . $sql . "<br>" . $conn->error;
+    echo "Error" . $sql . "<br>" . $conn->error;
 }
+
 echo "valmis";
 
+//$conn->close();
 ?>
